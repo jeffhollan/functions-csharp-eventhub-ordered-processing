b1d3aa6eb0c8f6993d665f6930fc3942a5d6038f
Jeff Hollan <jehollan@microsoft.com>
2018-03-13 23:00:01 -0700

    attempt at circuit in v2

--- OrderedEventHubs/EventHubTrigger.cs
+++ OrderedEventHubs/EventHubTrigger.cs
@@ -6,6 +6,7 @@ using Polly;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,14 +16,16 @@ namespace OrderedEventHubs
     {
         private static ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(Environment.GetEnvironmentVariable("Redis"));
         private static IDatabase db = redis.GetDatabase();
+        private static Policy _circuit;
+
         [FunctionName("EventHubTrigger")]
+        [Disable(typeof(CircuitStatus))]
         public static async Task RunAsync(
             [EventHubTrigger(eventHubName: "events", Connection = "EventHub")] EventData[] eventDataSet, 
             TraceWriter log,
             [Queue("deadletter")] IAsyncCollector<string> queue)
         {
             log.Info($"Triggered batch of size {eventDataSet.Length}");
-
             foreach (var eventData in eventDataSet) {
                 var result = await Policy
                 .Handle<Exception>()
@@ -30,6 +33,7 @@ namespace OrderedEventHubs
                 {
                     await db.ListRightPushAsync("events:" + context["partitionKey"], (string)context["counter"] + $"CAUGHT{retryCount}");
                 })
+                .WrapAsync(GetCircuit())
                 .ExecuteAndCaptureAsync(async () =>
                 {
                     if (int.Parse((string)eventData.Properties["counter"]) % 100 == 0)
@@ -46,6 +50,39 @@ namespace OrderedEventHubs
                     await queue.AddAsync(Encoding.UTF8.GetString(eventData.Body.Array));
                     await queue.FlushAsync();
                 }
+            }
+        }
+
+        private static Policy GetCircuit()
+        {
+            if(_circuit == null)
+            {
+                Action<Exception, TimeSpan> onBreak = (exception, timespan) => {
+                    CircuitStatus.Disable();
+                    File.SetLastWriteTimeUtc("host.json", DateTime.UtcNow);
+                };
+                Action onReset = () => {
+                    CircuitStatus.Enable();
+                    File.SetLastWriteTimeUtc("host.json", DateTime.UtcNow);
+                };
+                _circuit = Policy
+                    .Handle<Exception>()
+                    .CircuitBreakerAsync(10, new TimeSpan(0, 5, 0), onBreak, onReset);  
+            }
+            return _circuit;
+        }
+
+        private static class CircuitStatus
+        {
+            private static bool _status = bool.Parse(db.StringGet("disabled"));
+            public static bool IsDisabled() { return _status; }
+            public static void Disable() {
+                db.StringSet("disabled", "true");
+                _status = true;
+            }
+            public static void Enable() {
+                db.StringSet("disabled", "false");
+                _status = false;
             }
         }
     }
